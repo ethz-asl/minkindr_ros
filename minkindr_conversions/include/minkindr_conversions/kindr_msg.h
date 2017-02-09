0d6c5059c186cfaae9de1beefa9e55e6d8880c1d
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2017-02-09 11:11:05 +0100

    switched to templated functions to allow these functions to work with floats as well as doubles

--- minkindr_conversions/include/minkindr_conversions/kindr_msg.h
+++ minkindr_conversions/include/minkindr_conversions/kindr_msg.h
@@ -1,8 +1,6 @@
 #ifndef MINKINDR_CONVERSIONS_KINDR_MSG_H
 #define MINKINDR_CONVERSIONS_KINDR_MSG_H
 
-#include <Eigen/Core>
-#include <Eigen/Geometry>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -10,50 +8,149 @@
 #include <geometry_msgs/Transform.h>
 #include <geometry_msgs/Vector3.h>
 #include <kindr/minimal/quat-transformation.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <eigen_conversions/eigen_msg.h>
+#include <glog/logging.h>
 
 namespace tf {
 
 // Convert a kindr::minimal::QuatTransformation to a 6 DoF geometry msgs pose.
-void poseKindrToMsg(const kindr::minimal::QuatTransformation& kindr,
-                    geometry_msgs::Pose* msg);
-void poseMsgToKindr(const geometry_msgs::Pose& msg,
-                    kindr::minimal::QuatTransformation* kindr);
-void poseStampedKindrToMsg(const kindr::minimal::QuatTransformation& kindr,
-                    const ros::Time& time,
-                    const std::string& reference_frame,
-                    geometry_msgs::PoseStamped* msg);
-// Uses current time.
-void poseStampedKindrToMsg(const kindr::minimal::QuatTransformation& kindr,
-                    const std::string& reference_frame,
-                    geometry_msgs::PoseStamped* msg);
+template <typename Scalar>
+void poseKindrToMsg(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    geometry_msgs::Pose* msg) {
+  CHECK_NOTNULL(msg);
+  pointKindrToMsg(kindr.getPosition(), &msg->position);
+  quaternionKindrToMsg(kindr.getRotation(), &msg->orientation);
+}
 
-// Convert a kindr::minimal::QuatTransformation to a geometry_msgs::Transform.
-void transformKindrToMsg(const kindr::minimal::QuatTransformation& kindr,
-                         geometry_msgs::Transform* msg);
-void transformMsgToKindr(const geometry_msgs::Transform& msg,
-                         kindr::minimal::QuatTransformation* kindr);
+template <typename Scalar>
+void poseMsgToKindr(const geometry_msgs::Pose& msg,
+                    kindr::minimal::QuatTransformationTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<Scalar, 3, 1> position;
+  Eigen::Quaternion<Scalar> rotation;
+
+  quaternionMsgToKindr(msg.orientation, &rotation);
+  pointMsgToKindr(msg.position, &position);
+
+  *kindr =
+      kindr::minimal::QuatTransformationTemplate<Scalar>(rotation, position);
+}
+
+template <typename Scalar>
+void poseStampedKindrToMsg(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    const ros::Time& time, const std::string& reference_frame,
+    geometry_msgs::PoseStamped* msg) {
+  CHECK_NOTNULL(msg);
+  msg->header.frame_id = reference_frame;
+  msg->header.stamp = time;
+  poseKindrToMsg(kindr, &msg->pose);
+}
+
+// Uses current time.
+template <typename Scalar>
+void poseStampedKindrToMsg(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    const std::string& reference_frame, geometry_msgs::PoseStamped* msg) {
+  poseStampedKindrToMsg(kindr, ros::Time(), reference_frame, msg);
+}
+
 
 // A wrapper for the relevant functions in eigen_conversions.
-void quaternionKindrToMsg(const kindr::minimal::RotationQuaternion& kindr,
-                          geometry_msgs::Quaternion* msg);
-void quaternionMsgToKindr(const geometry_msgs::Quaternion& msg,
-                          kindr::minimal::RotationQuaternion* kindr);
+template <typename Scalar>
+void quaternionKindrToMsg(
+    const kindr::minimal::RotationQuaternionTemplate<Scalar>& kindr,
+    geometry_msgs::Quaternion* msg) {
+  CHECK_NOTNULL(msg);
+  quaternionEigenToMsg(kindr.toImplementation(), *msg);
+}
+
+template <typename Scalar>
+void quaternionMsgToKindr(
+    const geometry_msgs::Quaternion& msg,
+    kindr::minimal::RotationQuaternionTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaternion<Scalar> quat;
+  quaternionMsgToEigen(msg, quat);
+  *kindr = kindr::minimal::RotationQuaternionTemplate<Scalar>(quat);
+}
 
 // Also the Eigen implementation version of this.
-void quaternionKindrToMsg(const Eigen::Quaterniond& kindr,
-                          geometry_msgs::Quaternion* msg);
+template <typename Scalar>
+void quaternionKindrToMsg(const Eigen::Quaternion<Scalar>& kindr,
+                          geometry_msgs::Quaternion* msg) {
+  CHECK_NOTNULL(msg);
+  quaternionEigenToMsg(kindr, *msg);
+}
+
+template <typename Scalar>
 void quaternionMsgToKindr(const geometry_msgs::Quaternion& msg,
-                          Eigen::Quaterniond* kindr);
+                          Eigen::Quaternion<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaternion<double> kindr_double;
+  quaternionMsgToEigen(msg, kindr_double);
+  *kindr = kindr_double.cast<Scalar>();
+}
 
 // A wrapper for the relevant functions in eigen_conversions.
-void pointKindrToMsg(const Eigen::Vector3d& kindr, geometry_msgs::Point* msg);
-void pointMsgToKindr(const geometry_msgs::Point& msg, Eigen::Vector3d* kindr);
+template <typename Scalar>
+void pointKindrToMsg(const Eigen::Matrix<Scalar, 3, 1>& kindr,
+                     geometry_msgs::Point* msg) {
+  CHECK_NOTNULL(msg);
+  pointEigenToMsg(kindr, *msg);
+}
 
-void vectorKindrToMsg(const Eigen::Vector3d& kindr,
-                      geometry_msgs::Vector3* msg);
+template <typename Scalar>
+void pointMsgToKindr(const geometry_msgs::Point& msg,
+                     Eigen::Matrix<Scalar, 3, 1>* kindr) {
+  CHECK_NOTNULL(kindr);
+  pointMsgToEigen(msg, *kindr);
+}
 
+template <typename Scalar>
+void vectorKindrToMsg(const Eigen::Matrix<Scalar, 3, 1>& kindr,
+                      geometry_msgs::Vector3* msg) {
+  CHECK_NOTNULL(msg);
+  vectorEigenToMsg(kindr, *msg);
+}
+
+template <typename Scalar>
 void vectorMsgToKindr(const geometry_msgs::Vector3& msg,
-                      Eigen::Vector3d* kindr);
+                      Eigen::Matrix<Scalar, 3, 1>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<double, 3, 1> kindr_double;
+  vectorMsgToEigen(msg, kindr_double);
+  *kindr = kindr_double.cast<Scalar>();
+}
+
+// Convert a kindr::minimal::QuatTransformation to a geometry_msgs::Transform.
+template <typename Scalar>
+void transformKindrToMsg(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    geometry_msgs::Transform* msg) {
+  CHECK_NOTNULL(msg);
+  vectorKindrToMsg(kindr.getPosition(), &msg->translation);
+  quaternionKindrToMsg(kindr.getRotation(), &msg->rotation);
+}
+
+template <typename Scalar>
+void transformMsgToKindr(
+    const geometry_msgs::Transform& msg,
+    kindr::minimal::QuatTransformationTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<Scalar, 3, 1> position;
+  Eigen::Quaternion<Scalar> rotation;
+
+  quaternionMsgToKindr(msg.rotation, &rotation);
+  vectorMsgToKindr(msg.translation, &position);
+
+  *kindr =
+      kindr::minimal::QuatTransformationTemplate<Scalar>(rotation, position);
+}
 
 }  // namespace tf
 
--- minkindr_conversions/include/minkindr_conversions/kindr_tf.h
+++ minkindr_conversions/include/minkindr_conversions/kindr_tf.h
@@ -1,39 +1,116 @@
 #ifndef MINKINDR_CONVERSIONS_KINDR_TF_H
 #define MINKINDR_CONVERSIONS_KINDR_TF_H
 
-#include <Eigen/Core>
-#include <Eigen/Geometry>
 #include <kindr/minimal/quat-transformation.h>
 #include <tf/transform_datatypes.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <glog/logging.h>
+#include <tf_conversions/tf_eigen.h>
 
 namespace tf {
 
 // Convert a kindr::minimal::QuatTransformation to a 6 DoF geometry msgs pose.
-void poseKindrToTF(const kindr::minimal::QuatTransformation& kindr,
-                   tf::Pose* tf_type);
+template <typename Scalar>
+void poseKindrToTF(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    tf::Pose* tf_type) {
+  transformKindrToTF(kindr, tf_type);
+}
+
+template <typename Scalar>
 void poseTFToKindr(const tf::Pose& tf_type,
-                   kindr::minimal::QuatTransformation* kindr);
+                   kindr::minimal::QuatTransformationTemplate<Scalar>* kindr) {
+  transformTFToKindr(tf_type, kindr);
+}
 
 // Convert a kindr::minimal::QuatTransformation to a geometry_msgs::Transform.
-void transformKindrToTF(const kindr::minimal::QuatTransformation& kindr,
-                        tf::Transform* tf_type);
-void transformTFToKindr(const tf::Transform& tf_type,
-                        kindr::minimal::QuatTransformation* kindr);
+template <typename Scalar>
+void transformKindrToTF(
+    const kindr::minimal::QuatTransformationTemplate<Scalar>& kindr,
+    tf::Transform* tf_type) {
+  CHECK_NOTNULL(tf_type);
+  tf::Vector3 origin;
+  tf::Quaternion rotation;
+  vectorKindrToTF(kindr.getPosition(), &origin);
+  quaternionKindrToTF(kindr.getRotation(), &rotation);
+  tf_type->setOrigin(origin);
+  tf_type->setRotation(rotation);
+}
+
+template <typename Scalar>
+void transformTFToKindr(
+    const tf::Transform& tf_type,
+    kindr::minimal::QuatTransformationTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<Scalar, 3, 1> position;
+  Eigen::Quaternion<Scalar> rotation;
+
+  quaternionTFToKindr(tf_type.getRotation(), &rotation);
+  vectorTFToKindr(tf_type.getOrigin(), &position);
+
+  // Enforce positive w.
+  if (rotation.w() < 0) {
+    rotation.coeffs() = -rotation.coeffs();
+  }
+
+  *kindr =
+      kindr::minimal::QuatTransformationTemplate<Scalar>(rotation, position);
+}
 
 // A wrapper for the relevant functions in eigen_conversions.
-void quaternionKindrToTF(const kindr::minimal::RotationQuaternion& kindr,
-                         tf::Quaternion* tf_type);
-void quaternionTFToKindr(const tf::Quaternion& tf_type,
-                         kindr::minimal::RotationQuaternion* kindr);
+template <typename Scalar>
+void quaternionKindrToTF(
+    const kindr::minimal::RotationQuaternionTemplate<Scalar>& kindr,
+    tf::Quaternion* tf_type) {
+  CHECK_NOTNULL(tf_type);
+  quaternionEigenToTF(kindr.toImplementation(), *tf_type);
+}
+
+template <typename Scalar>
+void quaternionTFToKindr(
+    const tf::Quaternion& tf_type,
+    kindr::minimal::RotationQuaternionTemplate<Scalar>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaternion<Scalar> quat;
+  quaternionTFToEigen(tf_type, quat);
+  *kindr = kindr::minimal::RotationQuaternionTemplate<Scalar>(quat);
+}
 // Also the Eigen implementation version of this.
-void quaternionKindrToTF(const Eigen::Quaterniond& kindr,
-                         tf::Quaternion* tf_type);
+template <typename Scalar>
+void quaternionKindrToTF(const Eigen::Quaternion<Scalar>& kindr,
+                         tf::Quaternion* tf_type) {
+  CHECK_NOTNULL(tf_type);
+  quaternionEigenToTF(kindr, *tf_type);
+}
+
+template <typename Scalar>
 void quaternionTFToKindr(const tf::Quaternion& tf_type,
-                         Eigen::Quaterniond* kindr);
+                         Eigen::Quaternion<Scalar>* kindr) {
+
+  CHECK_NOTNULL(kindr);
+  Eigen::Quaterniond kindr_double;
+  quaternionTFToEigen(tf_type, kindr_double);
+  *kindr = kindr_double.cast<Scalar>();
+}
 
 // A wrapper for the relevant functions in eigen_conversions.
-void vectorKindrToTF(const Eigen::Vector3d& kindr, tf::Vector3* tf_type);
-void vectorTFToKindr(const tf::Vector3& tf_type, Eigen::Vector3d* kindr);
+template <typename Scalar>
+void vectorKindrToTF(const Eigen::Matrix<Scalar, 3, 1>& kindr,
+                     tf::Vector3* tf_type) {
+  CHECK_NOTNULL(tf_type);
+  vectorEigenToTF(kindr, *tf_type);
+}
+
+template <typename Scalar>
+void vectorTFToKindr(const tf::Vector3& tf_type,
+                     Eigen::Matrix<Scalar, 3, 1>* kindr) {
+  CHECK_NOTNULL(kindr);
+  Eigen::Matrix<double, 3, 1> kindr_double;
+  vectorTFToEigen(tf_type, kindr_double);
+  *kindr = kindr_double.cast<Scalar>();
+}
 
 }  // namespace tf
 
